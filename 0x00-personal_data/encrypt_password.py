aba1893d7431bab153ba7ef03cf4163dd76e40b8
Tianomiano <patomiano6@gmail.com>
2023-11-02 09:53:35 +0300

    validate hashed passord

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,11 @@ def hash_password(password: str) -> bytes:
     """Hashes a password randomly
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Use bcrypt to validate that the provided password
+    matches the hashed password..
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
