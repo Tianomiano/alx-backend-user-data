18c732fa8c6c2215b7be547e6cd5674ef991dfa8
Tianomiano <patomiano6@gmail.com>
2023-11-07 07:26:19 +0300

    401 error handler

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,11 +20,12 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ unauthorized handler
     """
-    return jsonify({"error": "Not found"}), 401
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
