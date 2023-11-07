39adb4c8d4d20b7fb14a4da0669b9e374e62d8a8
Tianomiano <patomiano6@gmail.com>
2023-11-07 07:48:34 +0300

    created the authentiication class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """manage api authentication
     """
@@ -11,12 +12,12 @@ class Auth:
         """check if path requires authentication
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """returns value in auth header
         """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """returns the current user
         """
