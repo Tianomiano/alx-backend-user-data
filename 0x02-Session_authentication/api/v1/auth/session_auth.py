be89267aa1210833c22a9e1fb2d505430006c3a7
Tianomiano <patomiano6@gmail.com>
2023-11-09 10:31:24 +0300

    create a session id for a user id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,6 +11,7 @@ class SessionAuth(Auth):
     first step for creating a new authentication mechanism
     """
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """
         creates a Session ID for a user_id
